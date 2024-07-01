e42d9fa3de84564016d57e16a680b61c7382e8a7
komalsai234 <99163734+Komalsai234@users.noreply.github.com>
2024-07-01 14:58:01 +0530

    dvc

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Covid-19-Chest-X-ray-Classifier"
+AUTHOR_USER_NAME = "komalsai234"
+SRC_REPO = "Covid_19"
+AUTHOR_EMAIL = "saianurag234@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python Packages for Covid-19 Chest X-ray Prediction",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
